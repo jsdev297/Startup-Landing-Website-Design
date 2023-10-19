48caf3b8db89d298fb8ee810c88ac4c920b7e9c6
Prapya <prapyabakshi123@gmail.com>
2023-10-19 05:53:51 -0700

    Header Responsive Done

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,7 @@
 const headerResponsiveBtn = document.querySelector("#res-btn");
+const navMenu = document.querySelector("#navMenu");
 
 headerResponsiveBtn.addEventListener("click", function () {
     this.classList.toggle("active");
+    navMenu.classList.toggle("active");
 });
