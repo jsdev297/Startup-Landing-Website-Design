6c53560693dca527c97df4ca0f2671b7c29ad0f1
Prapya <prapyabakshi123@gmail.com>
2023-10-19 21:22:37 -0700

    Sticky Header Done

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,25 @@
+// Menu Button
 const headerResponsiveBtn = document.querySelector("#res-btn");
+// Navbar
 const navMenu = document.querySelector("#navMenu");
+// Page Header
+const pageHeader = document.querySelector(".page-header")
 
+// Responsive Header
 headerResponsiveBtn.addEventListener("click", function () {
     this.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
+
+// Sticky Header
+window.onscroll = function () {
+    const scrollY = window.scrollY;
+    if (scrollY >= 100) {
+        pageHeader.classList.add("sticky");
+        console.log()
+    } else {
+        if (pageHeader.classList.contains("sticky")) {
+            pageHeader.classList.remove("sticky");
+        }
+    }
+}
